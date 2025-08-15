0d1a91c91e126eac8990b1b62885e5c0e27c25c1
wwsouthard <wsouthard@my365.bellevue.edu>
2025-08-15 15:45:35 -0500

    week 2 projects completed

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,25 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Will Southard
+      Date:   8/15/25
+
+      Filename: project02-02.js
+ */
+
+// form
+function verifyForm() {
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+
+      // Conditional to check for blank fields
+      let output = name && email && phone ? "Thank you!" : "Please fill in all fields";
+      window.alert(output);
+}
+
+//listener
+const button = document.getElementById("submit");
+button.addEventListener("click", verifyForm);
